1ff63dd17817be2c40c783202058c0188e90e7b2
Matt McCormick <matt.mccormick@kitware.com>
2021-04-28 17:39:03 -0400

    ENH: Initial commit

---
+++ napari_itk_io/__init__.py
@@ -0,0 +1,9 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+from ._reader import napari_get_reader
+from ._writer import napari_get_writer, napari_write_image
+
+
---
+++ napari_itk_io/_tests/test_writer.py
@@ -0,0 +1,3 @@
+# from napari_itk_io import napari_get_writer, napari_write_image
+
+# add your tests here...
