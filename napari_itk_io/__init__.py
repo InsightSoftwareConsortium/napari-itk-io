f42d9aea508171eb7dca0da2c8b0073a4c5f167d
ch-n <Nauroth_C@ukw.de>
2022-03-25 15:14:38 +0100

    Update to npe2

--- napari_itk_io/__init__.py
+++
@@ -1,7 +0,0 @@
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "unknown"
-
-from ._reader import napari_get_reader
-from ._writer import napari_get_writer, napari_write_image
